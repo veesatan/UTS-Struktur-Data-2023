c4b238a903f4003d169c30adbf292736a642bb40
veesatan <134835504+veesatan@users.noreply.github.com>
2023-10-04 07:02:56 +0800

    Delete soal_03.py

--- soal_03.py
+++
@@ -1,9 +0,0 @@
-def gabung_list(list1, list2):
-    return list1 + list2
-
-# Contoh penggunaan
-list_a = [1, 2, 3]
-list_b = [4, 5, 6]
-
-hasil_gabungan = gabung_list(list_a, list_b)
-print("Hasil gabungan list: ", hasil_gabungan)
